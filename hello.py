a27849517df0dd01c31294fa9dab1bc7fcc94940
Owen Tan <2601837@sit.singaporetech.edu.sg>
2026-09-04 10:54:45 +0800

    Follwer Growth Tracker

--- hello.py
+++ hello.py
@@ -3,10 +3,22 @@ print("Welcome here")
 print("My first post!")
 print("===============================")
 
-username = "cool_creator"
-bio = "Fun Blogger"
+username = input("Enter Username:")
+age = input("Enter Age:")
+category = input("Enter Content Category:")
 followers = 100
 
+followers += 50
+print("Day 1:", followers)
+
+followers += 20
+print("Day 2:", followers)
+
+followers += 10
+print("Day 3:", followers)
+
+print("\nInstagram Profile")
+print("================================")
 print("Username:", username)
-print("Bio:", bio)
-print("Followers:", followers)
+print("Age:", age)
+print("Category:", category)
