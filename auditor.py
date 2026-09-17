73254adddee677f919daae1d775b27868b8f9cbb
Owen Tan <2601837@sit.singaporetech.edu.sg>
2026-09-18 00:39:11 +0800

    Added generate_report function

--- auditor.py
+++ auditor.py
@@ -5,10 +5,6 @@ def get_valid_input(user_input, failed_entries):
     if user_input.isdigit() == False:
         print("\nInvalid input. Please enter a valid stock quantity.\n")
         failed_entries += 1
-    #else:
-    #    inventory += int(user_input)
-    #    print("Current stock quantity: ", inventory)
-
     return failed_entries
 
 def process_delivery(current_total,new_value):
@@ -19,14 +15,13 @@ def calculate_tax(inventory):
     tax = 0.1 * inventory
     return tax
 
+def generate_report(total_inventory,failed_entries):
+    print("\n\nTotal units processed: ", total_inventory, "units.")
+    print("Number of Failed/Rejected Entries: ", failed_entries)
+    print("Total tax to be paid: $", f"{calculate_tax(total_inventory):.2f}","\n")
+
 quit = input("Please enter stock quantity or type 'quit' to exit: ")
 while quit != "quit":
-   # if quit.isdigit() == False:
-   #     print("\nInvalid input. Please enter a valid stock quantity.\n")
-   #     i += 1
-   # else:
-   #     inventory += int(quit)
-   #     print("Current stock quantity: ", inventory)
    i = get_valid_input(quit, i)
    inventory = process_delivery(inventory, int(quit) if quit.isdigit() else 0)
    print ("Current delivery processed: ", inventory)
@@ -39,6 +34,4 @@ while quit != "quit":
         break
    quit = input("\nPlease enter stock quantity or type 'quit' to exit: ")
 
-print("\n\nTotal units processed: ", inventory, "units.")
-print("Number of Failed/Rejected Entries: ", i)
-print("Total tax to be paid: $", f"{calculate_tax(inventory):.2f}","\n")
+generate_report(inventory, i)
